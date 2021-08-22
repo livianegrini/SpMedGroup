4c4333854fbea3ec87db032d689ff7ea42d57dbc
livianegrini <liviasnegrini2004@gmail.com>
2021-08-22 13:26:32 -0300

    DDL e DML Finalizadas

---
+++ Scripts/Scripts_SpMedGroup-DDL.sql
@@ -0,0 +1,138 @@
+CREATE DATABASE SPMEDICALGROUP_Livia;
+GO
+
+USE SPMEDICALGROUP_Livia;
+GO
+
+CREATE TABLE TipoUsuario(
+   IdTipoUsuario INT PRIMARY KEY IDENTITY(1,1),
+   TipoUsuario VARCHAR(20) NOT NULL UNIQUE
+);
+GO
+
+CREATE TABLE Usuario(
+   IdUsuario INT PRIMARY KEY IDENTITY(1,1),
+   IdTipoUsuario INT FOREIGN KEY REFERENCES TipoUsuario(IdTipoUsuario),
+   Email VARCHAR(200) NOT NULL UNIQUE,
+   Senha VARCHAR(20) NOT NULL 
+);
+GO
+
+CREATE TABLE Endereco(
+   IdEndereco INT PRIMARY KEY IDENTITY(1,1),
+   Logadouro VARCHAR(70) NOT NULL,
+   Numero SMALLINT NOT NULL,
+   Bairro VARCHAR(50) NOT NULL,
+   Municipio VARCHAR(50) NOT NULL,
+   Estado VARCHAR(50) NOT NULL,
+   Cep INT NOT NULL
+);
+GO
+
+CREATE TABLE Clinica(
+   IdClinica INT PRIMARY KEY IDENTITY(1,1),
+   IdEndereco INT FOREIGN KEY REFERENCES Endereco(IdEndereco),
+   HorarioFuncionamento TIME NOT NULL,
+   Cnpj INT NOT NULL UNIQUE,
+   NomeFantasia VARCHAR(70) NOT NULL,
+   RazaoSocial  VARCHAR(70) NOT NULL
+);
+GO
+
+CREATE TABLE Especialidade(
+   IdEspecialidade INT PRIMARY KEY IDENTITY(1,1),
+   Especialidade VARCHAR(50) NOT NULL
+);
+GO
+
+CREATE TABLE Paciente(
+   IdPaciente INT PRIMARY KEY IDENTITY(1,1),
+   IdUsuario INT FOREIGN KEY REFERENCES Usuario(IdUsuario),
+   IdEndereco INT FOREIGN KEY REFERENCES Endereco(IdEndereco),
+   Nome VARCHAR(70) NOT NULL,
+   DataNascimento DATE NOT NULL,
+   Telefone INT,
+   Rg INT NOT NULL UNIQUE,
+   Cpf INT NOT NULL UNIQUE
+);
+GO
+
+CREATE TABLE Medico(
+   IdMedico INT PRIMARY KEY IDENTITY(1,1),
+   IdUsuario INT FOREIGN KEY REFERENCES Usuario(IdUsuario),
+   IdClinica INT FOREIGN KEY REFERENCES Clinica(IdClinica),
+   IdEspecialidade INT FOREIGN KEY REFERENCES Especialidade(IdEspecialidade),
+   Crm INT NOT NULL UNIQUE,
+   Nome VARCHAR(70) NOT NULL
+);
+GO
+
+CREATE TABLE Situacao(
+   IdSituacao INT PRIMARY KEY IDENTITY(1,1),
+   TipoSituacao VARCHAR(30) NOT NULL
+);
+GO
+
+CREATE TABLE Consulta(
+   IdConsulta INT PRIMARY KEY IDENTITY(1,1),
+   IdPaciente INT FOREIGN KEY REFERENCES Paciente(IdPaciente),
+   IdMedico INT FOREIGN KEY REFERENCES Medico(IdMedico),
+   IdSituacao INT FOREIGN KEY REFERENCES Situacao(IdSituacao),
+   [Data] DATE NOT NULL,
+   Hora TIME NOT NULL
+);
+GO
+
+
+
+--ERROS
+DROP TABLE Medico;
+DROP TABLE Consulta;
+DROP TABLE Clinica;
+DROP TABLE Paciente;
+
+
+CREATE TABLE Clinica(
+   IdClinica INT PRIMARY KEY IDENTITY(1,1),
+   IdEndereco INT FOREIGN KEY REFERENCES Endereco(IdEndereco),
+   HorarioInicio TIME NOT NULL,
+   HorarioFim TIME NOT NULL,
+   Cnpj CHAR(60) NOT NULL UNIQUE,
+   NomeFantasia VARCHAR(70) NOT NULL,
+   RazaoSocial  VARCHAR(70) NOT NULL
+);
+GO
+
+CREATE TABLE Medico(
+   IdMedico INT PRIMARY KEY IDENTITY(1,1),
+   IdUsuario INT FOREIGN KEY REFERENCES Usuario(IdUsuario),
+   IdClinica INT FOREIGN KEY REFERENCES Clinica(IdClinica),
+   IdEspecialidade INT FOREIGN KEY REFERENCES Especialidade(IdEspecialidade),
+   Crm VARCHAR(50) NOT NULL UNIQUE,
+   Nome VARCHAR(70) NOT NULL
+);
+GO
+
+CREATE TABLE Consulta(
+   IdConsulta INT PRIMARY KEY IDENTITY(1,1),
+   IdPaciente INT FOREIGN KEY REFERENCES Paciente(IdPaciente),
+   IdMedico INT FOREIGN KEY REFERENCES Medico(IdMedico),
+   IdSituacao INT FOREIGN KEY REFERENCES Situacao(IdSituacao),
+   DataCon DATE NOT NULL,
+   Hora TIME NOT NULL
+);
+GO
+
+CREATE TABLE Paciente(
+   IdPaciente INT PRIMARY KEY IDENTITY(1,1),
+   IdUsuario INT FOREIGN KEY REFERENCES Usuario(IdUsuario),
+   IdEndereco INT FOREIGN KEY REFERENCES Endereco(IdEndereco),
+   Nome VARCHAR(70) NOT NULL,
+   DataNascimento DATE NOT NULL,
+   Telefone CHAR(11),
+   Rg INT NOT NULL UNIQUE,
+   Cpf CHAR(11) NOT NULL UNIQUE
+);
+GO
+
+
